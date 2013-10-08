60bc0b7caa54217f75fe310691562d89298578ee
JM Addington <jm@jmaddington.com>
2013-10-08 14:23:13 -0400

    Rewrote PostMessage function according to Jamie's gist
    Removed unneeded code
    Added forceResize option
    
    Gist: https://gist.github.com/jameymcelveen/6793503

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ TheCity.PluginHelper.resizeIFrame({
   subdomain: 'www',    //Change this to your City subdomain
   useSSL: false,       //Whether or not your plugin uses SSL
   extra: 50,           //Extra number of pixels to expand iFrame height to 
-                        (newHeight= documentHeight + extra)
-  refresh: 250,        //How often to check for new documentHeight, 0 to disable
+                       //(newHeight= documentHeight + extra)
+  refresh: 250,        //How often to check for new documentHeight, 0 to disable,
+  forceResize: false  //By default the plugin doesn't resize if the height is within
+                      //75px of the last time it was called. Set to true to override
 });
 ```
--- tcplugin.helper.js
+++ tcplugin.helper.js
@@ -6,33 +6,11 @@ TheCity.PluginHelper = function() {
         subdomain: 'www',
         useSSL: false,
         extra: 50,
-        refresh: 250
+        refresh: 250,
+        forceResize: false
     });
 
-    // Cross Domain Post Message
-    var cache_bust = 1, window = this;
-    var crossDomainPostMessage = function(message, target_url, target) {
-        if (!target_url) return;
-        target = target || parent;
-        if (window['postMessage']){
-            target['postMessage'](message, target_url.replace( /([^:]+:\/\/[^\/]+).*/, '$1'));
-            return true;
-        }
-        else if (target_url){
-            target.location = target_url.replace(/#.*$/, '') + '#' + (+new Date) + (cache_bust++) + '&' + message;
-            return true;
-        }
-    };
-
     return {
-        // resize the containing IFrame to be tall enough to display all the content
-        // in the child document
-        initPlugin: function(subdomain) {
-            var src = 'https://' + subdomain + '.onthecity.org/#' + encodeURIComponent(document.location.href);
-            var documentHeight = $(document).height();
-            crossDomainPostMessage(documentHeight, src, frames[0]);
-        },
-
         resizeIFrame: function(options) {
             //Merge previous settings w/ passed options
             var settings = $.extend(
@@ -40,20 +18,27 @@ TheCity.PluginHelper = function() {
                 options
             );
 
+            var body = document.body;
+            var html = document.documentElement;
+            var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight);
+
             //Check to see if height has changed since last call -- if NOT and height > 0
             //This prevents an infinite loop where we keep expanding the iFrame size. We test for a difference of
             // > 50 because (at least on Chrome) an extra ~30 pixels are added to the height AFTER we've stored the new height
-            var diff = Math.abs($(document).data('TheCity.PluginHelper.documentHeight') - $(document).height()) ;
-            if ( diff < 50 && $(document).height()) {
+            var diff = Math.abs($(document).data('TheCity.PluginHelper.height') - height) ;
+            if ( diff < 75 && $(document).height() && !settings.forceResize) {
                 return;
             }
 
             //Overwrite saved settings with new ones
             $(document).data('TheCity.PluginHelper', settings);
             var schema = settings.useSSL ? "https" : "http";
-            var src = schema + '://' + settings.subdomain + '.onthecity.org/#' + encodeURIComponent(document.location.href);
-            if (crossDomainPostMessage($(document).height() + settings.extra, src, frames[0]) === true && settings.refresh !=0){
-                $(document).data('TheCity.PluginHelper.documentHeight', $(document).height());
+            var url = schema + '://' + settings.subdomain + '.onthecity.org/#' + encodeURIComponent(document.location.href);
+            var target = target || parent;
+            target.postMessage(height + settings.extra, url);
+            if (settings.refresh != 0){
+                var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight);
+                $(document).data('TheCity.PluginHelper.height', height);
                 setInterval(function(){TheCity.PluginHelper.resizeIFrame();}, settings.refresh);
             }
         }
